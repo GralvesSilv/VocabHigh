9ec62a03f0fffd29e14f5e2a5568c1ba22c61685
Gabriel <gabrielalves322313>
2024-05-19 19:06:40 -0300

    removendo

--- test_imports.py
+++
@@ -1,6 +0,0 @@
-from VocabHigh.models.Card import Card
-from VocabHigh.models.Deck import Deck
-from VocabHigh.services.Chat_ini import Chat_ini
-from VocabHigh.config import Ai_settings
-
-print("Imports funcionaram corretamente!")
